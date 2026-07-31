80f2b160383f7daa43bcee596847bc22bdb25e0c
Sanchos-dev <sanchos@sanchos.su>
2026-07-31 03:34:06 +0300

    simple installer flow

--- utils.py
+++ utils.py
@@ -3,19 +3,30 @@ class installer():
     #I HATE "self" IT IS SO ANNOYING
     import shutil
     import subprocess
-    def check_package_existance(self, package):
+    def check_package_existence(self, package):
         return self.shutil.which(package) is not None
 
-    def install(self, package): #this goes first
+    def install(self, package):
         print(f"trying to install: {package}")
-        if self.check_package_existance(package) == True:
+        if self.check_package_existence(package) == True:
             print(f"Package {package} already in the system. Aborting.")
         else:
-            try:
-                print("Trying pacman...")
-                self.subprocess.run(["sudo", "pacman", "-S", f"{package}"])
-            except Exception as e:
-            	print(e)
+            
+            print("Trying pacman...")
+            res = self.subprocess.run(["sudo", "pacman", "-S", package])
+            if res.returncode != 0: #we're sayin "fuck it" and trying AUR
+                print(f" no {package} in pacman, trying yay")
+                if self.check_package_existence("yay") == True:
+                	res1 = self.subprocess.run(["yay", "-S", f"{package}"])
+                	if res1.returncode != 0:
+                		print(f"\n\n\n SUCCESFULLY INSTALLED {package} WITH YAY")
+            else:
+            	print(f"\n\n\n SUCCESFULLY INSTALLED {package} WITH PACMAN")
+
+
+
+
+
 
 
 
